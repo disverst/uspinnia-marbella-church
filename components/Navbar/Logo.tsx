fafdb8c7ee7b27ebd9886eda34e9649e9f2426af
disverst <disverst@gmail.com>
2023-06-30 21:38:15 +0200

    Commented LogoUS

--- components/Navbar/Logo.tsx
+++ components/Navbar/Logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import * as React from 'react';
 import { Box, Typography } from '@mui/material';
 import { TabRoutes } from 'components/Navbar/NavbarConstants';
-import LogoUS from 'components/Navbar/LogoUS.svg';
+// import LogoUS from 'components/Navbar/LogoUS.svg';
 
 const Logo: React.FC = () => (
   <Typography
@@ -15,7 +15,7 @@ const Logo: React.FC = () => (
   >
     <Link href={TabRoutes.mainUrl}>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <LogoUS />
+        {/* <LogoUS /> */}
         <Typography variant="body2" sx={{ ml: 1, fontSize: '16px' }}>
           Успіння Божої Матері
         </Typography>
