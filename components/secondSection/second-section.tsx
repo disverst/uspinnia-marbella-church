dd112b39212d32023d803db896bae2603fbef3bd
disverst <disverst@gmail.com>
2023-09-19 08:11:33 +0200

    Media query for events

--- components/secondSection/second-section.tsx
+++ components/secondSection/second-section.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import EventList from 'components/events/event-list';
+import Button from 'components/ui/button';
+import AddressIcon from 'components/icons/address-icon';
 import classes from './second-section.module.css';
-import { getAllEvents } from '../../dummy-data';
+import { getLastThreeEvents } from '../../dummy-data';
 
 export default function SecondSection() {
-  const events = getAllEvents();
+  const events = getLastThreeEvents();
+  const exploreLink = `/events`;
   return (
     <section className={classes.section}>
       <div className={classes.container}>
         <h1>Життя нашої Парафії</h1>
         <EventList items={events} />
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Усі події</span>
+            <span className={classes.icon}>
+              <AddressIcon />
+            </span>
+          </Button>
+        </div>
       </div>
     </section>
   );
