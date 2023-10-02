05edc4d66924222d4b9d30c544592d841ba2ba0d
disverst <disverst@gmail.com>
2023-10-02 11:13:11 +0200

    Created footer section with contacts

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import FirstSection from 'components/firstSection/first-section';
 import SecondSection from 'components/secondSection/second-section';
+import FooterSection from 'components/zFooterSection/footer-section';
 
 export default function Home() {
   return (
     <>
       <FirstSection />
       <SecondSection />
+      <FooterSection />
     </>
   );
 }
